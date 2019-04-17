576df3f6b7dba73baca27703d82f1ddcb7ef3537
Alexandre DUHEM <alexandreduhem@gmail.com>
2019-04-17 17:06:29 +0200

    fix the media_id call: call with GET request because POST leads on error 405 Method not allowed

--- InstagramAPI/InstagramAPI.py
+++ InstagramAPI/InstagramAPI.py
@@ -271,7 +271,7 @@ class InstagramAPI:
                            '_uid': self.username_id,
                            '_csrftoken': self.token,
                            'media_id': mediaId})
-        return self.SendRequest('media/' + str(mediaId) + '/info/', self.generateSignature(data))
+        return self.SendRequest('media/' + str(mediaId) + '/info/')
 
     def explore(self):
         return self.SendRequest('discover/explore/')
